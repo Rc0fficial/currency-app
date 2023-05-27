ff6f64100cd068cf9aec193778579a7ed3063c3f
rc0fficial <thunder9905@gmail.com>
2023-05-27 17:06:11 +0500

    update

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,57 @@
 <template>
   <div class="container mx-auto mt-6">
     <h1 class="font-bold text-3xl">Search Old Currency</h1>
-    <div>
-      <label for="countryFilter">Country:</label>
-      <select id="countryFilter" v-model="selectedCountry">
-        <option value="">Select Country</option>
-        <option v-for="country in countries" :value="country">
-          {{ country }}
-        </option>
-      </select>
-    </div>
-    <div>
-      <label for="yearFilter">Filter by Year:</label>
-      <select id="yearFilter" v-model="selectedYear">
-        <option value="">All</option>
-        <option v-for="year in years" :value="year">{{ year }}</option>
-      </select>
-    </div>
-    <div class="mb-4">
-      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" @click="clearFilters">Clear Filters</button>
+    <div class="flex items-center justify-between mb-12 gap-12">
+      <div class="flex gap-6">
+        <div class="relative">
+          <label for="countryFilter" class="block">Country</label>
+          <select
+            id="countryFilter"
+            v-model="selectedCountry"
+            class="block appearance-none w-full px-4 py-2 border border-gray-300 rounded-md"
+          >
+            <option value="">Select Country</option>
+            <option
+              v-for="country in countries"
+              :value="country"
+              class="border-t border-gray-300"
+            >
+              {{ country }}
+            </option>
+            <option value="">All Countries</option>
+          </select>
+          <div
+            class="absolute inset-y-0 right-0 flex items-center px-2 pointer-events-none"
+          >
+            <ChevronDownIcon class="w-4 h-4 text-gray-500" />
+          </div>
+        </div>
+        <div class="relative">
+          <label for="yearFilter" class="block">Year</label>
+          <select
+            id="yearFilter"
+            v-model="selectedYear"
+            class="block appearance-none w-full px-4 py-2 border border-gray-300 rounded-md"
+          >
+            <option value="">All years</option>
+            <option
+              v-for="year in years"
+              :value="year"
+              class="border-t border-gray-300"
+            >
+              {{ year }}
+            </option>
+          </select>
+        </div>
+      </div>
+      <div class="mb-4">
+        <button
+          class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          @click="clearFilters"
+        >
+          Clear All
+        </button>
+      </div>
     </div>
     <div class="grid grid-cols-3 gap-4">
       <CurrencyCard
@@ -34,10 +67,12 @@
 import { defineComponent } from "vue";
 import CurrencyCard from "./components/CurrencyCard.vue";
 import currencyData from "./currencies.json";
+import ChevronDownIcon from "@/components/icons/ChevronDownIcon.vue";
 
 export default defineComponent({
   components: {
     CurrencyCard,
+    ChevronDownIcon,
   },
   data() {
     return {
@@ -48,7 +83,9 @@ export default defineComponent({
   },
   computed: {
     countries() {
-      return [...new Set(this.currencies.map((currency) => currency.country_name))];
+      return [
+        ...new Set(this.currencies.map((currency) => currency.country_name)),
+      ];
     },
     years() {
       return [...new Set(this.currencies.map((currency) => currency.year))];
@@ -56,7 +93,8 @@ export default defineComponent({
     filteredCurrencies() {
       return this.currencies.filter((currency) => {
         return (
-          (!this.selectedCountry || currency.country_name === this.selectedCountry) &&
+          (!this.selectedCountry ||
+            currency.country_name === this.selectedCountry) &&
           (!this.selectedYear || currency.year === this.selectedYear)
         );
       });
