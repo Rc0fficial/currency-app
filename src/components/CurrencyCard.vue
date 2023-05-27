6faf0017be60453273a415368933ea54ada4be9d
rc0fficial <thunder9905@gmail.com>
2023-05-27 18:17:43 +0500

    update

--- src/components/CurrencyCard.vue
+++ src/components/CurrencyCard.vue
@@ -1,13 +1,13 @@
 <template>
   <div
-    class="currency-card border-2 border-solid rounded-lg overflow-hidden shadow-2xl border-slate-200"
+    class="currency-card border-2 border-solid rounded-lg overflow-hidden shadow-xl border-slate-200"
   >
     <img :src="currency.front_pic" alt="Front" class="currency-card-image" />
     <div class="mx-3 mt-4 mb-6">
       <h3 class="font-bold text-slate-600">{{ currency.country_name }}</h3>
-      <p class="text-slate-600 text-xs font-semibold">{{ currency.year }}</p>
-      <p class="text-slate-600 text-xs font-semibold">{{ currency.status }}</p>
-      <p class="text-xs text-slate-600">{{ currency.history }}</p>
+      <p class="text-slate-600 text-sm font-semibold">{{ currency.year }}</p>
+      <p class="text-slate-600 text-sm font-semibold">{{ currency.status }}</p>
+      <p class="text-sm text-slate-600">{{ currency.history }}</p>
     </div>
   </div>
 </template>
