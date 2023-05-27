ef8e70ae9467c9a37f05c26ccc4b7e17cb9e6400
rc0fficial <thunder9905@gmail.com>
2023-05-27 18:26:58 +0500

    update

--- src/components/CurrencyCard.vue
+++ src/components/CurrencyCard.vue
@@ -2,11 +2,28 @@
   <div
     class="currency-card border-2 border-solid rounded-lg overflow-hidden shadow-xl border-slate-200"
   >
-    <img :src="currency.front_pic" alt="Front" class="currency-card-image" />
+    <img
+      :src="currency.front_pic"
+      alt="Front"
+      class="currency-card-image h-[150px]"
+    />
     <div class="mx-3 mt-4 mb-6">
-      <h3 class="font-bold text-slate-600">{{ currency.country_name }}</h3>
-      <p class="text-slate-600 text-sm font-semibold">{{ currency.year }}</p>
-      <p class="text-slate-600 text-sm font-semibold">{{ currency.status }}</p>
+      <div class="flex justify-between">
+        <div class="flex flex-col">
+          <h3 class="font-bold text-slate-600">{{ currency.country_name }}</h3>
+          <p class="text-slate-600 text-sm font-normal">
+            {{ currency.year }}
+          </p>
+        </div>
+        <button
+          class="bg-blue-500 hover:bg-blue-700 text-white py-1 px-2 rounded"
+        >
+          Bid
+        </button>
+      </div>
+      <p class="text-slate-600 text-sm my-2 font-normal">
+        {{ currency.status }}
+      </p>
       <p class="text-sm text-slate-600">{{ currency.history }}</p>
     </div>
   </div>
